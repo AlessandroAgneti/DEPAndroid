be1a2aeaa5408ab76d8578af55e1048eebb9785e
AlessandroAgneti <alex.agneti@gmail.com>
2019-01-31 15:04:25 +0100

    First Login page

--- app/src/main/java/com/example/depeat/MainActivity.java
+++ app/src/main/java/com/example/depeat/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.depeat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edtPassword, edtemail;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
